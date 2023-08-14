e6750b3ac2aa727980ad71f450291ded6fb9d3f9
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-14 15:16:47 +0200

    main

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import axios from 'axios'
+  var a = 1
+  function f() {
+    axios
+      .post("//localhost:3000/server", {c: a})
+      .then(resp => {
+        a = resp.data.c
+      })
+  }
+</script>
+<button on:click={f}>Post data</button>
+{a}
---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(cors())
+app.use('/', express.static(__dirname +'/../client/build'))
+
+app.post('/server', (req, res) => {
+  res.json({x: 2, c: req.body.c + 1})
+})
+
+app.get('/server', (req, res) => {
+  res.json({x: 3})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
