c00774de0ed260fe0645dc518569b4e115c650cd
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-14 20:54:55 +0200

    .

--- server/index.js
+++ server/index.js
@@ -1,24 +1,22 @@
 const express = require('express')
 const cors = require('cors')
-const app = express()
-const port = 3000
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('db.db')
 
+var app = express()
 app.use(express.json())
 app.use(cors())
 app.use('/', express.static(__dirname +'/../client/build'))
 
+const db = new sqlite3.Database('db.db')
 app.post('/server', (req, res) => {
-  un = req.body.un
-  pw = req.body.pw
   db.all(
     "SELECT * FROM user WHERE un = ? and pw = ?",
-    [un, pw],
+    [req.body.un, req.body.pw],
     (err, data) => data[0] && res.json({name: data[0].name})
   )
 })
 
+const port = 3000
 app.listen(port, () => {
-  console.log(`- http://localhost:${port}`)
+  console.log(`Szerver elindítva:\thttp://localhost:${port}`)
 })
