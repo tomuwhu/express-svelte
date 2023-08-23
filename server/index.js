bebf8694ec02028b0a098236cdaf0e6387cc3e32
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-23 20:59:27 +0200

    .

--- server/index.js
+++ server/index.js
@@ -5,14 +5,18 @@ const sqlite3 = require('sqlite3').verbose()
 var app = express()
 app.use(express.json())
 app.use(cors())
-app.use('/', express.static(__dirname +'/../client/build'))
+app.use('/', express.static(__dirname + '/../client/build'))
+
+app.get('/test', (req, res) => {
+  res.send('test succesful')
+})
 
 const db = new sqlite3.Database('db.db')
 app.post('/server', (req, res) => {
   db.all(
     "SELECT * FROM user WHERE un = ? and pw = ?",
     [req.body.un, req.body.pw],
-    (err, data) => data[0] && res.json({name: data[0].name})
+    (err, data) => data[0] && res.json({ name: data[0].name })
   )
 })
 
